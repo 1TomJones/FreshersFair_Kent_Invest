b19ea6790c68dac7d95808ce5cdb8d82e78c691d
1TomJones <jonesthomas2004@yahoo.co.uk>
2025-09-25 21:17:03 +0100

    Update ui-mobile.js

--- ui-mobile.js
+++ ui-mobile.js
@@ -1,8 +1,10 @@
-function UIMobile() {
-  return React.createElement("div", { className: "p-4" },
-    React.createElement("h1", { className: "text-xl font-bold" }, "Mobile Game Layout"),
-    React.createElement("p", null, "Here will go the simplified UI for mobile.")
-  );
-}
-
-window.UIMobile = UIMobile;
+(function () {
+  function UIMobile() {
+    return React.createElement("div", { className: "p-3 space-y-3" },
+      React.createElement("h1", { className: "text-xl font-bold" }, "Mobile Game Layout"),
+      React.createElement("div", { className: "p-3 rounded-xl border bg-white" }, "This is the mobile UI placeholder."),
+      React.createElement(window.Chart || (() => React.createElement("div", null, "Chart missing")))
+    );
+  }
+  window.UIMobile = UIMobile;
+})();
