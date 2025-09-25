539960cc393543e780967830fdd4120df70fa00a
1TomJones <jonesthomas2004@yahoo.co.uk>
2025-09-25 21:16:47 +0100

    Update ui-desktop.js

--- ui-desktop.js
+++ ui-desktop.js
@@ -1,8 +1,10 @@
-function UIDesktop() {
-  return React.createElement("div", { className: "p-4" },
-    React.createElement("h1", { className: "text-2xl font-bold" }, "Desktop Game Layout"),
-    React.createElement("p", null, "Here will go the trading UI for desktop.")
-  );
-}
-
-window.UIDesktop = UIDesktop;
+(function () {
+  function UIDesktop() {
+    return React.createElement("div", { className: "max-w-5xl mx-auto p-4 space-y-4" },
+      React.createElement("h1", { className: "text-2xl font-bold" }, "Desktop Game Layout"),
+      React.createElement("div", { className: "p-3 rounded-xl border bg-white" }, "This is the desktop UI placeholder."),
+      React.createElement(window.Chart || (() => React.createElement("div", null, "Chart missing")))
+    );
+  }
+  window.UIDesktop = UIDesktop;
+})();
